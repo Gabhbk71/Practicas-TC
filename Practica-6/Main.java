07490ee53214e4552de733467d7ae5fe8b146ce9
Gab_hbk <gabrielhbk64@hotmail.com>
2024-10-22 00:05:46 -0600

    Practica-6

---
+++ Practica-6/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                            new InputStreamReader(System.in));
+        StreamTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        while (t-- > 0){
+
+        }
+    }
+}
+
+
+
+                   /*pegar esto en la consola
+                   23,5
+                   15,4
+                   15,10
+                   29,8
+                   18,12
+                   */
+
+                   //ejercicio
+                   //https://omegaup.com/arena/problem/Llamadas-telefonicas/
+
+
